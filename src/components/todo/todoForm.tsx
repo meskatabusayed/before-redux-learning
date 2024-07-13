04ebc14ac5eb3a5f0c5c8917832186450845358b
meskatabusayed <meskatabusayed@gmail.com>
2024-07-13 20:50:27 +0530

    todo List create

--- src/components/todo/todoForm.tsx
+++ src/components/todo/todoForm.tsx
@@ -1,12 +1,29 @@
-import { useContext } from "react"
+import { FormEvent, useContext, useState } from "react"
 import { todoContext } from "../../context/todoProvider"
 
 
 const TodoForm = () => {
-    const {todoTitle} = useContext(todoContext)
+    const {state , dispatch} = useContext(todoContext);
+    console.log(state);
+    const [task , setTask] = useState(" ");
+    const handleSubmit = (e : FormEvent) => {
+        e.preventDefault();
+        const todo = {
+            id : Math.random().toString(36).substring(2,7),
+            title : task,
+            isCompleted : false,
+        }
+        dispatch({type : "addTodo" , payload : todo});
+    }
   return (
     <div>
-      <h1>{todoTitle}</h1>
+      <h1>Add Todo</h1>
+      <form onSubmit={handleSubmit}>
+      <label htmlFor="todo">Task</label>
+      <input className="border border-red-500"
+       onChange={(e) => setTask(e.target.value)} type="text" name="todo" id="todo"/>
+      <button className="bg-purple-800 p-2 text-white rounded-md" type="submit">Submit</button>
+      </form>
     </div>
   )
 }
--- src/context/todoProvider.tsx
+++ src/context/todoProvider.tsx
@@ -1,5 +1,10 @@
 /* eslint-disable react-refresh/only-export-components */
+
 import { ReactNode, createContext, useReducer } from "react";
+export const todoContext = createContext<{state : TTodo[] , dispatch : React.Dispatch<TAction> } | undefined>(undefined);
+
+/* //dispatch : React.Dispatch<TAction> ata  othoba state : TTodo[] type to show korlo na...... */
+
 type todoProviderProps = {
     children : ReactNode
 }
@@ -12,22 +17,22 @@ type TAction = {
     type : string;
     payload : TTodo
 }
-export const todoContext = createContext<{state : TTodo[] , dispatch : React.Dispatch<TAction> } | undefined>(undefined);
-//dispatch : React.Dispatch<TAction> ata  othoba state : TTodo[] type to show korlo na......
+
+
 const initialState : TTodo[] = [];
+    
 const reducer = (currentSate : TTodo[] , action : TAction) => {
     switch (action.type) {
         case "addTodo":
             return [...currentSate , action.payload];
-            
-    
         default:
             currentSate;
     }
 }
 
 const TodoProvider = ({children} : todoProviderProps) => {
-    const {state , dispatch} = useReducer(reducer , initialState);
+    const [state , dispatch] = useReducer(reducer , initialState);
+    
     const values = {
        state,
        dispatch
